e34732ad61b9538f6f4c77b29baaf1be0d243077
Dheeraj Gavendra <148362358+Tyson0001@users.noreply.github.com>
2026-07-19 11:21:36 +0530

    Add: n-th-tribonacci-number - submission-5

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public: 
+    vector<int>dp;
+    int tribonacci(int n) {
+        dp.assign(n+1,-1);
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 1;
+        dp[0]=0,dp[1]=1,dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
+    }
+    int solve(int n){
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1; 
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+    }
+};
